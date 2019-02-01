cf99117254329bd4a85fe5f4cd4232f370d13c15
IIITBIZZLEIII <IIITBIZZLEIII@users.noreply.github.com>
2019-02-01 19:41:03 -0400

    Added source file

--- SimpC++.cpp
+++ SimpC++.cpp
@@ -1,25 +1,25 @@
 #include <iostream>;
-#include namespace std;
+using namespace std;
 
 int main()
 {
 	int age(0);
 	string choice="";
-	string name=""
-	cout << " What is your name ? " << endl
+	string name="";
+	cout << " What is your name ? " << endl;
 	cin >> name;
 	cout<< " How old are you ? " << endl;
 	cin >> age;
 	cout << " Do chickens lay eggs... y/n? " << endl;
 	cin >> choice;
 	
-	if choice = "y" || "Y"
+	if (choice = "y" )
 	{
 		cout << "Correct !" << endl;
 	}
-	else if choice = "n" || "N"
+	else if (choice = "n")
 	{
-		cout << Incorrect !" << endl;
+		cout << "Incorrect !" << endl;
 	}
 	
 	return 0;
